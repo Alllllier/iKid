cc73f4a0aec28108c1dce7efea525b958f23e973
Ally <allier0416@gmail.com>
2018-10-27 17:55:52 -0700

    Initial Commit

---
+++ iKid/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iKid
+//
+//  Created by Jue Chen on 10/27/18.
+//  Copyright © 2018 Jue Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ iKid/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iKid
+//
+//  Created by Jue Chen on 10/27/18.
+//  Copyright © 2018 Jue Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
